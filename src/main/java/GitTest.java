e2aee30c823a1d59fe32872e8ac86244c02fdf4f
kongmutian <130917877+kongmutian@users.noreply.github.com>
2023-04-18 22:20:42 +0800

    Update GitTest.java

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("git test4");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
